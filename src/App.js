816c5c311083f51902ebefabd7720118106e8fc3
Kevin Salinas <salinaskevin031@gmail.com>
2025-05-23 15:43:58 -0500

    Añado la paginas para que integren las funcionalidades

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Home from 'containers/pages/Home';
 import store from './store';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { Provider } from 'react-redux';
+import Catalogo from 'containers/pages/Catalogo';
+import Vender from 'containers/pages/Vender';
+import Mis_Compras from 'containers/pages/Mis_Compras';
+import Sing_Up from 'containers/pages/Sing_Up';
+import Login from 'containers/pages/Login';
 
 function App() {
   return (
@@ -12,7 +17,12 @@ function App() {
       {/* Error Display */}
       <Route path='*' element={<Error404/>}/>
       {/* Error Display */}
-      <Route path='/' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/catalogo' element={<Catalogo />}/>
+      <Route path='/vender' element={<Vender/>}/>
+      <Route path='/mis_Compras' element={<Mis_Compras/>}/>
+      <Route path='/registrarse' element={<Sing_Up/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
   </Router>
     </Provider>
